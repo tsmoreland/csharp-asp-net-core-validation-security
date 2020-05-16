2dfcfd2fd1888c3767a6844e574abae75d3bf1d2
Terry Moreland <tsmoreland@live.com>
2020-05-16 14:48:46 +0100

    2.1 adding attributes to speaker class properties, guessing at maximum length for stringlength attributes

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -6,15 +6,27 @@ namespace ConferenceTracker.Entities
 {
     public class Speaker
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         [Display(Name = "First name")]
+        [DataType(DataType.Text)]
+        [StringLength(256)]
         public string FirstName { get; set; }
+        [Required]
         [Display(Name = "Last name")]
+        [StringLength(256)]
+        [DataType(DataType.Text)]
         public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [StringLength(2048)]
         public string Description { get; set; }
         [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
         [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
         public bool IsStaff { get; set; }
     }
